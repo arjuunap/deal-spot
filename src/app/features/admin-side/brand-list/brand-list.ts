5760e45cdde789d30870a45dc10841343cbe7068
arjun <arjunap7785@gmail.com>
2026-07-01 09:58:10 +0530

    edited style on brand edit

--- src/app/features/admin-side/brand-list/brand-list.ts
+++ src/app/features/admin-side/brand-list/brand-list.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { BrandService } from '../../../core/admin-side/Services/brand/brand';
 import { Router } from '@angular/router';
 import { environment } from '../../../../environment/environment';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-brand-list',
@@ -77,17 +78,44 @@ export class BrandList {
     this.router.navigate(['/admin-side/brand']);
   }
 
-  deleteBrand(brandId: number): void {
-    this.brandService.deleteBrand(brandId).subscribe({
-      next: () => {
-        console.log('Brand deleted successfully');
-        this.fetchBrands();
-      },
-      error: (err) => {
-        console.error('Error deleting brand:', err);
-      }
-    });
-  }
+//   deleteBrand(brandId: number): void {
+//   Swal.fire({
+//     title: 'Are you sure?',
+//     text: 'You won\'t be able to undo this action!',
+//     icon: 'warning',
+//     showCancelButton: true,
+//     confirmButtonColor: '#d33',
+//     cancelButtonColor: '#6c757d',
+//     confirmButtonText: 'Yes, delete it!',
+//     cancelButtonText: 'Cancel',
+//     reverseButtons: true
+//   }).then((result) => {
+//     if (result.isConfirmed) {
+//       this.brandService.deleteBrand(brandId).subscribe({
+//         next: () => {
+//           Swal.fire({
+//             icon: 'success',
+//             title: 'Deleted!',
+//             text: 'Brand has been deleted successfully.',
+//             timer: 1500,
+//             showConfirmButton: false
+//           });
+
+//           this.fetchBrands();
+//         },
+//         error: (err) => {
+//           console.error('Error deleting brand:', err);
+
+//           Swal.fire({
+//             icon: 'error',
+//             title: 'Delete Failed',
+//             text: 'Unable to delete the brand. Please try again.'
+//           });
+//         }
+//       });
+//     }
+//   });
+// }
 
   onEdit(brandId: number): void {
     this.router.navigate(['/admin-side/edit-brand', brandId]);
